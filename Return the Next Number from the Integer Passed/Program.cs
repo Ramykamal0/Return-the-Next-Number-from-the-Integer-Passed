7f6407bbdbbe2034e802f387c001d82767ea7d0c
Ramykamal0 <ramykamal012@gmail.com>
2026-06-07 19:39:15 +0300

    Initial commit

---
+++ Return the Next Number from the Integer Passed/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+using System;
+namespace ReturntheNextNumber
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number : .... ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" number = {addition(num)}");
+        }
+
+
+        static int addition(int num)
+        {
+            return num + 1;
+        }
+    }
+}
